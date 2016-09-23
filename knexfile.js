a44d71b174c2b12712cd21cb4511fed2186b3559
johnmberger <johnmartinberger@gmail.com>
2016-09-23 12:03:13 -0600

    updated knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -20,5 +20,14 @@ module.exports = {
     seeds: {
       directory: __dirname + '/src/server/db/seeds'
     }
-  }
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    }
 };
