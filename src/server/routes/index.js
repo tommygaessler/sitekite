c7e788bccfeae0ec981904c51d5539279fe85b5b
AustinMahan <amahan0424@gmail.com>
2016-09-26 12:47:29 -0600

    routes refactored

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const knex = require('../db/knex');
 const passportGithub = require('../auth/github');
 const {get, addUser, checkForms, userInDb, checkNewUser, getProjects, compareUser} = require('../queries/index');
-
+const authHelpers = require('../auth/helpers');
 const indexController = require('../controllers/index');
 const ghPinnedRepos = require('gh-pinned-repos');
 
@@ -14,30 +14,24 @@ router.get('/', function (req, res, next) {
 router.get('/:username', function (req, res, next) {
   userInDb(req.params)
   .then((data) => data ? res.render('admin_home_page.html', data[0]) : res.send(data))
-  .catch((error) => {
-    console.log(error);
-  });
+  .catch((error) => console.log(error));
 });
 
 router.get('/:username/projects', function (req, res, next) {
   userInDb(req.params)
   .then(getProjects)
-  .then((data) => data ? res.render('admin_projects_page.html', data[0]) : res.send('Error'));
+  .then((data) => data ? res.render('admin_projects_page.html', data[0]) : res.send('Error'))
+  .catch((error) => console.log(error));
 });
 
 router.get('/:userName/contact', function (req, res, next) {
   const username = req.params.userName;
   knex('users').where('username', username)
-  .then((user) => {
-    const renderObject = user[0];
-    res.render('admin_contact_page.html', renderObject);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+  .then((user) => res.render('admin_contact_page.html', user[0]))
+  .catch((error) => console.log(error));
 });
 
-router.get('/:userName/dashboard', passportGithub.authenticate('github', { failureRedirect: '/' }), function (req, res, next) {
+router.get('/:userName/dashboard', authHelpers.authRequired, function (req, res, next) {
   var user1 = req.params.userName
   var user2 = req.user.username
   compareUser(user1, user2) ? res.render('dashboard', {title: 'dashboard'}) : res.render('error');
