83b86852b6958769f2ad6c9244ce6e1434c4a218
AustinMahan <amahan0424@gmail.com>
2016-09-27 08:02:23 -0600

    changed migration file

--- src/server/db/migrations/20160913095210_users.js
+++ src/server/db/migrations/20160913095210_users.js
@@ -1,6 +1,16 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('users', (table) => {
     table.increments();
+    table.string('username').notNullable().unique();
+    table.string('github_token').notNullable().unique();
+    table.string('name');
+    table.string('email');
+    table.string('twitter');
+    table.string('linkedin');
+    table.string('profile_pic_url');
+    table.string('theme_name');
+    table.text('bio_desc');
+    table.text('contact_desc');
   });
 };
 
