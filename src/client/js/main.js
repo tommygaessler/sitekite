43af1b1e5c9d60855ca4ee398cc01ea19293996b
johnmberger <johnmartinberger@gmail.com>
2016-09-27 13:22:16 -0600

    added fun animation to github buttons

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -7,6 +7,12 @@
     $('.dropdown-toggle').on('click', function () {
       $(this).toggleClass('active');
     });
+    $('.github-signin').on('click', function () {
+      $(this).animate({
+        width: '72.1406px'
+      });
+      $(this).html('<i class="fa fa-github fa-spin fa-fw"></i>');
+    });
   });
 
 })();
