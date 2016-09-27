dc03a4947ebaea8a428e42120ac1abb1409beb60
AustinMahan <amahan0424@gmail.com>
2016-09-27 10:18:13 -0600

    the things do a thing

--- src/client/js/dashboard.js
+++ src/client/js/dashboard.js
@@ -1,5 +1,16 @@
-$(document).on('change', 'select', function() {
-  var id = $('select').val();
-  $('.pinned-project-info').hide();
-  $(`div[id*='${id}']`).show();
-});
+// $(document).on('change', 'select', function() {
+//   var id = $('select').val();
+//   $('.pinned-project-info').hide();
+//   $(`div[id*='${id}']`).show();
+// });
+
+$(document).ready(function() {
+  $('select').change(function() {
+    var thingname = $(this).val().split('/')[1];
+    console.log(thingname);
+    $('.pinned-project-info').fadeOut(100);
+    setTimeout(function() {
+      $(`#${thingname}`).fadeIn(100)
+    }, 100)
+  })
+})
