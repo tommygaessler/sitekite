fd4d7a081636e2112446aaf1f9682823549b9bbe
AustinMahan <amahan0424@gmail.com>
2016-09-28 09:10:48 -0600

    the flashing thingy bugg

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -18,23 +18,23 @@ router.get('/:username', function (req, res, next) {
   userInDb(req.params)
   .then((data) => {return loggedInUser(req, data)})
   .then((data) => data.length ? res.status(202).render('home.html', data[0]) :  res.status(404).render('error', {message: 'No User Found', status: 404}))
-  .catch((error) => console.log(error));
+  .catch((error) => res.send(error));
 });
 
 router.get('/:username/projects', function (req, res, next) {
   userInDb(req.params)
   .then(getProjects)
   .then((data) => {return loggedInUser(req, data)})
-  .then((data) => data ? res.status(202).render('projects.html', data[0]) : res.status(404).render('error'))
-  .catch((error) => console.log(error));
+  .then((data) => data ? res.status(202).render('projects.html', data[0]) : res.status(404).render('error', {message: 'No User Found', status: 404}))
+  .catch((error) => res.send(error));
 });
 
 router.get('/:userName/contact', function (req, res, next) {
   const username = req.params.userName;
   knex('users').where('username', username)
   .then((data) => {return loggedInUser(req, data)})
-  .then((user) => res.status(202).render('contact.html', user[0]))
-  .catch((error) => console.log(error));
+  .then((data) => data.length ? res.status(202).render('contact.html', data[0]) : res.status(404).render('error', {message: 'No User Found', status: 404}))
+  .catch((error) => res.send(error));
 });
 
 router.get('/:userName/dashboard', authHelpers.authRequired, function (req, res, next) {
@@ -44,7 +44,7 @@ router.get('/:userName/dashboard', authHelpers.authRequired, function (req, res,
   .then((data) => {
     compareUser(user1, user2) ? res.render('dashboard', {loggedInUser: req.user, username: req.user.username, pinnedProjects: data[0].projects}) : res.render('error');
   })
-  // .catch((err) => console.log(err));
+  .catch((err) => res.send(err));
 });
 
 router.post('/new', function (req, res, next) {
@@ -57,15 +57,16 @@ router.post('/new', function (req, res, next) {
 });
 
 router.post('/editPro', authHelpers.authRequired, function (req, res, next) {
-  console.log(req.user);
   updatePro(req.body)
   .then(() => res.redirect(`/${req.user.username}/dashboard`))
 })
 
-router.delete('/:username', function (req, res, next) {
+router.delete('/:username', authHelpers.authRequired, function (req, res, next) {
+  var user1 = req.params.userName
+  var user2 = req.user.username
   req.logout()
   removeUser(req.params.username)
-  .then(() => res.send('winning'))
+  .then(() => compareUser(user1, user2) ? res.send('winning') : res.render('error', {message: 'You aren\'t authorized '}))
 })
 
 module.exports = router;
