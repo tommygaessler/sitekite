be551f3481a94df245a9c1ee491ed7fb08910a9b
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-09-28 11:54:41 -0600

    made edit projects ui

--- src/client/js/dashboard.js
+++ src/client/js/dashboard.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
   $('.edit-project').on('click', function() {
+    $('.project-text').html('Edit Project');
     var thingname = $(this).attr('id');
-    console.log(thingname);
-    // $('.pinned-project-info').fadeOut(1);
-    // setTimeout(function() {
-    //   $(`#${thingname}`).fadeIn(1)
-    // }, 1)
+    $('.new-project').css('display', 'block');
     $('.pinned-project-info').css('display', 'none');
     $(`.pinned-project-info#${thingname}`).css('display', 'block');
+  })
+
+  $('.new-project').on('click', function() {
+    $('.project-text').html('Add Project');
+    $('.pinned-project-info').css('display', 'none');
+    $(`.pinned-project-info#manual`).css('display', 'block');
+    $(this).css('display', 'none');
   })
 
   $('.delete_account').click(function () {
