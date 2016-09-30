9b40f6ec4c343b3e69a180f77c53df913fc5d93f
johnmberger <johnmartinberger@gmail.com>
2016-09-30 09:43:15 -0600

    making this contact form work

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -59,37 +59,34 @@ $(document).ready(function () {
   $('#sendgrid').on('submit', function(event) {
     event.preventDefault();
 
-    if ($('#name').val() && $('#to_email').val() && $('#to_email').val()) {
+    document.getElementById('email_submit').innerHTML = '<i class="fa fa-circle-o-notch fa-spin fa-fw"></i>';
+    $('#email_submit').addClass('disabled');
 
-      document.getElementById('email_submit').innerHTML = '<i class="fa fa-circle-o-notch fa-spin fa-fw"></i>';
-      $('#email_submit').addClass('disabled');
+    const username = $(this).data('id')
 
-      const username = $(this).data('id')
-
-      const data = {
-        username: username,
-        name: $('#name').val(),
-        to_email: $('#to_email').val(),
-        from_email: $('#from_email').val(),
-        message: $('#to_email').val()
-      }
-
-      $.ajax({
-        method: 'POST',
-        url: `/${username}/contact/send`,
-        data: data
-      }).done((data) => {
-        $('#contact_desc').css('display', 'none');
-        $('#sendgrid').css('display', 'none');
-        $('#success-message').css('display', 'block');g
-        $('#success-message').text(data.message);
-      }).fail((error) => {
-        $('#fail-message').css('display', 'block');
-        $('#fail-message').text('Message was not sent, please try again');
-        document.getElementById('email_submit').innerHTML = 'Submit';
-        $('#email_submit').removeClass('disabled');
-      })
+    const data = {
+      username: username,
+      name: $('#name').val(),
+      to_email: $('#to_email').val(),
+      from_email: $('#from_email').val(),
+      message: $('#to_email').val()
     }
+
+    $.ajax({
+      method: 'POST',
+      url: `/${username}/contact/send`,
+      data: data
+    }).done((data) => {
+      $('#contact_desc').css('display', 'none');
+      $('#sendgrid').css('display', 'none');
+      $('#success-message').css('display', 'block');g
+      $('#success-message').text(data.message);
+    }).fail((error) => {
+      $('#fail-message').css('display', 'block');
+      $('#fail-message').text('Message was not sent, please try again');
+      document.getElementById('email_submit').innerHTML = 'Submit';
+      $('#email_submit').removeClass('disabled');
+    });
   });
 
 });
--- src/client/js/themes/john/john.js
+++ src/client/js/themes/john/john.js
@@ -3,5 +3,5 @@ $(document).ready(function() {
   $('.navbar-toggle').on('click', function () {
     $(this).toggleClass('active');
   });
-  
-}
+
+});
