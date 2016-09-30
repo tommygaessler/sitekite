3c72db876c186ef6945682e0184181d4da54b6a8
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-09-30 09:06:29 -0600

    refactored sendgrid email to use ajax

--- src/server/routes/contact.js
+++ src/server/routes/contact.js
@@ -4,11 +4,13 @@ const knex = require('../db/knex');
 var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
 
 router.post('/:username/contact/send', (req, res, next) => {
+
+  var name = req.body.name;
   var username = req.params.username;
   var helper = require('sendgrid').mail;
   var from_email = new helper.Email(req.body.from_email);
   var to_email = new helper.Email(req.body.to_email);
-  var subject = `New Message From ${req.body.name}`;
+  var subject = `New Message From ${name}`;
   var content = new helper.Content('text/plain', `${req.body.message}`);
   var mail = new helper.Mail(from_email, subject, to_email, content);
 
@@ -19,7 +21,7 @@ router.post('/:username/contact/send', (req, res, next) => {
   });
 
   sg.API(request, function(error, response) {
-    res.redirect(`/${username}/contact`);
+    res.json({message: `${name}, thanks for reaching out! I will get back to you asap!`});
   });
 });
 
